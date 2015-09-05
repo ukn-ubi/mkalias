2fe12dc2c0b5b0724ed828602fa4d35cff06e1ba
XiKuuKy <simon@phillipharms.com>
2015-09-05 18:00:50 +0800

    Actual initial commit.

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cxfreeze mkalias.py --target-name=mkalias --target-dir=bin/
---
+++ mkalias.py
@@ -0,0 +1,19 @@
+import sys
+
+try:
+    shell = sys.argv[1]
+    alias = sys.argv[2]
+    run = sys.argv[3]
+
+    run = run.replace('"', "")
+    run = run.replace("'", "")
+except:
+    print("Usage: \n\tmkalias <shell> <alias> <command>")
+    sys.exit()
+
+if shell == "zsh" or shell == "bash":
+    print("alias " + alias + "=\"" + run + "\"")
+elif shell == "fish":
+    print("alias " + alias + " \"" + run + "\"")
+else:
+    print("Sorry you're shell isn't supported. Please submit a issue on Github and I will try to include it.")
